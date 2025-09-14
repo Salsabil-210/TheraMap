e0aabf9ff851fd1650ea285384fc43bc214692a8
salsabil <sosokhaled528@gmail.com>
2025-09-14 23:29:36 +0300

    added createuser in UserRepository

--- Backend/repository/user.go
+++ Backend/repository/user.go
@@ -1,10 +1,27 @@
 package repository
 
 import (
-	"context"
 	"Backend/types"
+	"context"
+
+	"gorm.io/gorm"
 )
 
 type UserRepository interface {
 	CreateUser(ctx context.Context, user *types.UserModel) (*types.UserModel, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
+}
+
+func (r *userRepository) CreateUser(ctx context.Context, user *types.UserModel) (*types.UserModel, error) {
+	if err := r.db.WithContext(ctx).Create(user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
 }
